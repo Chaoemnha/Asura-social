00fd0a1fcef771bfc653363c68f71bb36e1bbd65
Chaoemnha <luannguyentm99@gmail.com>
2025-06-26 16:46:24 +0700

    finish

--- asura-backend/src/modules/index/index.route.ts
+++ asura-backend/src/modules/index/index.route.ts
@@ -1,17 +1,19 @@
 import { Router } from "express";
 //import { Route } from "../../core/interfaces"; Nhin qua dai
-import { Route } from "@core/interfaces";   
+import { Route } from "@core/interfaces";
 import IndexController from "./index.controller";
 
-export default class IndexRoute implements Route{
-public path = '/';
-public router = Router();
-//Trong Nodejs ta co the thoai mai new vi ta co the unit test duoc, huong thu tuc
-public indexController = new IndexController();
-constructor(){
+export default class IndexRoute implements Route {
+  public path = "/";
+  public router = Router();
+  //Trong Nodejs ta co the thoai mai new vi ta co the unit test duoc, huong thu tuc
+  public indexController = new IndexController();
+  constructor() {
     this.initializeRoutes();
-}
-private initializeRoutes(){
-    this.router.get(this.path, this.indexController.index);//Chinh la path = '/'; voi cu phap giong app.get('/', (req, res) ...)
-}
+  }
+  private initializeRoutes() {
+    this.router.get(this.path, (req, res) => {
+      res.redirect("/swagger/#/");
+    });
+  }
 }
